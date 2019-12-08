49d9986a0f534129e200222167794914d0828abf
Prismary <prismarymail@gmail.com>
2019-12-09 00:39:53 +0100

    Added Twitter functionality

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import datetime
 import os
 import time
 import twitter as t
+from shutil import copyfile
 
 with open('tokens.txt', 'r') as tokens_file:
 	for line in tokens_file:
@@ -51,7 +52,10 @@ def load():
 	print(pf('i')+'Data loaded.')
 
 def process(msg):
-	if msg.content.lower().startswith('add;'):
+	if msg.content == '?':
+		return '__**Command-Usage**__\n```Markdown\n# Add a song to the recommendation pool:\n\nadd;<artist>;<title>;<link>\n\n\n# Post a recommendation to Twitter:\n\nrec;[random/;<artist>;<title>]\n```'
+
+	elif msg.content.lower().startswith('add;'):
 		try:
 			r_artist = msg.content.split(';')[1]
 			r_title = msg.content.split(';')[2]
@@ -60,9 +64,45 @@ def process(msg):
 			return 'The message format is invalid. Type \'?\' for help.'
 
 		with open('data/pending/'+r_artist+'_'+r_title+'.txt', 'w') as r_file:
-			r_file.write(r_link)
+			r_file.write('artist;'+r_artist+'\n')
+			r_file.write('title;'+r_title+'\n')
+			r_file.write('link;'+r_link+'\n')
 
-		return 'Successfully added '+r_artist+' - '+r_title+' to the recommendation pool.'
+		return 'Successfully added **'+r_artist+' - '+r_title+'** to the recommendation pool.'
+
+	elif msg.content.lower().startswith('rec;'):
+		if msg.content.lower().split(';')[1] == 'random':
+			for files in os.walk('data/pending'):
+				filename = files[2][random.randrange(0, len(files[2])+1)]
+
+			with open('data/pending/'+filename, 'r') as r_file:
+				for line in r_file:
+					if line.startswith('artist;'):
+						r_artist = line.split(';')[1].replace('\n', '')
+					elif line.startswith('title;'):
+						r_title = line.split(';')[1].replace('\n', '')
+					elif line.startswith('link;'):
+						r_link = line.split(';')[1].replace('\n', '')
+		else:
+			try:
+				r_artist = msg.content.split(';')[1]
+				r_title = msg.content.split(';')[2]
+			except:
+				return 'The message format is invalid. Type \'?\' for help.'
+
+			try:
+				with open('data/pending/'+r_artist+'_'+r_title+'.txt', 'r') as r_file:
+					for line in r_file:
+						if line.startswith('link;'):
+							r_link = line.split(';')[1].replace('\n', '')
+			except:
+				return 'The specified file does not exist.'
+
+		print(pf('Tweet')+t.tweet(''+r_artist+' - '+r_title+'\n\n'+r_link))
+		copyfile('data/pending/'+r_artist+'_'+r_title+'.txt', 'data/posted/'+r_artist+'_'+r_title+'.txt')
+		os.remove('data/pending/'+r_artist+'_'+r_title+'.txt')
+		print(pf('i')+'> File transferred from /pending/ to /posted/.')
+		return 'Successfully posted **'+r_artist+' - '+r_title+'** to Twitter.'
 
 	else:
 		return 'Unable to process the message. Type \'?\' for help.'
@@ -92,7 +132,7 @@ async def on_message(message):
 			load()
 			await channel.send('`'+pf('i')+'Data successfully reloaded.'+'`')
 		elif message.content.lower().split(' ')[0] == '.tweet':
-			t.tweet(message.content[7:])
+			print(pf('Tweet')+t.tweet(message.content[7:]))
 			await channel.send('`'+pf('Tweet')+'Twitter status update called.'+'`')
 		elif message.content.lower().split(' ')[0] == '.whitelist':
 			if message.content.lower().split(' ')[1] == 'on':
