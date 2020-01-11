de5d4f2710043447936a1963230cb8f851d378ad
Prismary <prismarymail@gmail.com>
2020-01-12 00:44:59 +0100

    Updated to work with SQL

--- post_random.py
+++ post_random.py
@@ -1,22 +1,40 @@
 import twitter as t
-import os
-import random
-from shutil import copyfile
+import time
+import sqlite3
 
-for files in os.walk('data/pending'):
-    filename = files[2][random.randrange(0, len(files[2])+1)]
+conn = sqlite3.connect('taki.db')
+conn.create_function('REGEXP', 2, lambda x, y: 1 if re.search(x,y) else 0)
+cursor = conn.cursor()
 
-with open('data/pending/'+filename, 'r') as r_file:
-    for line in r_file:
-        if line.startswith('artist;'):
-            r_artist = line.split(';')[1].replace('\n', '')
-        elif line.startswith('title;'):
-            r_title = line.split(';')[1].replace('\n', '')
-        elif line.startswith('link;'):
-            r_link = line.split(';')[1].replace('\n', '')
+cursor.execute(
+    '''SELECT * FROM "main.Songs"
+    WHERE SongID IN (SELECT SongID FROM "main.Songs" WHERE Posted IS NULL ORDER BY RANDOM() LIMIT 1);'''
+)
 
-print(t.tweet(r_artist+' - '+r_title+'\n\n'+r_link))
-copyfile('data/pending/'+r_artist+'_'+r_title+'.txt', 'data/posted/'+r_artist+'_'+r_title+'.txt')
-os.remove('data/pending/'+r_artist+'_'+r_title+'.txt')
-print('> File transferred from /pending/ to /posted/.')
-print('Successfully posted '+r_artist+' - '+r_title+' to Twitter.')
+rows = cursor.fetchone()
+
+try:
+    r_songid = rows[0]
+    r_artist = rows[1]
+    r_title = rows[2]
+    r_posted = rows[3]
+except:
+    print('> No suitable songs have been found in the database.')
+    exit()
+
+cursor.execute('''
+    SELECT l.Link,lt.TypeName FROM "main.Links" AS l
+    JOIN "main.LinkTypes" AS lt ON l.LinkTypeID = lt.LinkTypeID
+    WHERE SongID = ? AND TypeName = "YouTube";''', (r_songid,)
+)
+r_links = cursor.fetchone()
+r_link = r_links[0]
+
+print(t.tweet(''+r_artist+' - '+r_title+'\n\n'+r_link))
+
+cursor.execute(
+    '''UPDATE "main.Songs"
+    SET Posted = ?
+    WHERE SongID = ?;''', (time.time(), r_songid)
+)
+conn.commit()
